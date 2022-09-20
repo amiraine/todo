e5f3e603c7d8a17eb2a14c89671ef2de00f9b87a
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 13:01:24 -0700

    start writing list component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,1 +1,48 @@
 import React from "react";
+import { BaseMap, ListItem } from "../../types";
+import DragAndDrop from "./DragAndDrop";
+import {
+  Checkbox,
+  StyledListItem,
+  StyledTextInput,
+} from "./DragAndDrop/styled";
+
+interface ListProps {
+  tasks: BaseMap<ListItem>;
+}
+const List: React.FC<ListProps> = (props) => {
+  const { tasks } = props;
+  return (
+    <DragAndDrop
+      dragVariants={{
+        drag: {
+          zIndex: 99,
+          position: "relative",
+          transition: {
+            duration: 0,
+          },
+        },
+        drop: {
+          zIndex: 0,
+        },
+      }}
+      positionTransition={{
+        type: "spring",
+        damping: 15,
+        stiffness: 100,
+      }}
+    >
+      {tasks.sort.map((taskId, i) => {
+        const { value, isDone } = tasks.items[taskId];
+        return (
+          <StyledListItem>
+            <StyledTextInput type="text" value={value} />
+            <Checkbox type="checkbox" checked={isDone} />
+          </StyledListItem>
+        );
+      })}
+    </DragAndDrop>
+  );
+};
+
+export default List;
