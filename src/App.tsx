777b2decf3a59eecac34910619090e6390d33008
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 11:48:24 -0700

    seed initial data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { Book, Container, Page } from "./styled";
+import { initialData } from "./initialdata";
+// import { v4 } from "uuid";
 const App = () => {
+  const [listData, setListData] = useState<any>();
+
+  useEffect(() => {
+    setListData(initialData);
+  }, []);
+
   return (
     <Container>
       <Book>
-        <Page orientation="left" />
-        <Page orientation="right" />
+        <Page orientation="left"></Page>
+        <Page orientation="right"></Page>
       </Book>
     </Container>
   );
