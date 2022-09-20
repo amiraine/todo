55c8f0b754d1a82cccdc17358ceb8c42933f45a3
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 13:50:29 -0700

    fix styled directory

--- src/components/List/styled.ts
+++ src/components/List/styled.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  padding: 0 24px 0 0;
+`;
+
 export const StyledListItem = styled.div`
   display: flex;
   justify-content: space-between;
@@ -9,6 +13,8 @@ export const StyledListItem = styled.div`
 export const StyledTextInput = styled.input`
   border: 0;
   background: transparent;
+  font-family: inherit;
+  font-size: 16px;
 `;
 
 export const Checkbox = styled.input``;
