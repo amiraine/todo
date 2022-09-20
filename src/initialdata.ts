52e62925a16fa90725d85e0749d52399445ff5b9
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 11:43:40 -0700

    seed initial data

---
+++ src/initialdata.ts
@@ -0,0 +1,21 @@
+import { BaseMap, ListItem } from "./types";
+
+export const initialData: BaseMap<ListItem> = {
+  items: {
+    "a711664b-e246-4108-a058-84fe00fb98a1": {
+      id: "a711664b-e246-4108-a058-84fe00fb98a1",
+      value: "Water plants",
+      isDone: true,
+    },
+    "648ff21c-16c3-4e9d-b67a-97a78f8acaca": {
+      id: "648ff21c-16c3-4e9d-b67a-97a78f8acaca",
+      value: "Feed cats",
+      isDone: false,
+    },
+  },
+  sort: [
+    "a711664b-e246-4108-a058-84fe00fb98a1",
+    "648ff21c-16c3-4e9d-b67a-97a78f8acaca",
+  ],
+  selected: "",
+};
