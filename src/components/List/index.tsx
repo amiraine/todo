5c15e40af439e10a82eeacf1e5aed45ec0c24a51
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 14:35:18 -0700

    start writing handler functions

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,15 +1,49 @@
 import React from "react";
-import { BaseMap, ListItem } from "../../types";
+import { v4 } from "uuid";
+import { useListData } from "../../Context";
+import { ListData, ListItem } from "../../types";
 import DragAndDrop from "./DragAndDrop";
 import { Checkbox, Container, StyledListItem, StyledTextInput } from "./styled";
 
-interface ListProps {
-  tasks: BaseMap<ListItem>;
-}
-const List: React.FC<ListProps> = (props) => {
-  const {
-    tasks: { items, sort, selected },
-  } = props;
+interface ListProps {}
+const List: React.FC<ListProps> = () => {
+  const [listData, listDispatch] = useListData();
+  const { items, sort, selected } = listData;
+
+  // helper functions
+  const handleAddNewItem = () => {
+    const newItem: ListItem = {
+      id: v4(),
+      value: "",
+      isDone: false,
+    };
+
+    const payload: ListData = {
+      items: { ...items, [newItem.id]: newItem },
+      sort: [...sort, newItem.id],
+      selected: newItem.id,
+    };
+
+    listDispatch({ type: "ADD", payload });
+  };
+
+  const handleUpdateItemValue = (id: string, newValue: string) => {
+    const itemCopy: ListItem = { ...items[id], value: newValue };
+    const payload: ListData = {
+      ...listData,
+      items: { ...items, [id]: itemCopy },
+    };
+    listDispatch({ type: "UPDATE", payload });
+  };
+
+  const handleSelectItem = (id: string) => {
+    listDispatch({ type: "SETACTIVE", payload: id });
+  };
+
+  const handleDeleteItem = (id: string) => {
+    listDispatch({ type: "REMOVE", payload: id });
+  };
+
   return (
     <Container>
       <DragAndDrop
@@ -31,11 +65,11 @@ const List: React.FC<ListProps> = (props) => {
           stiffness: 100,
         }}
       >
-        {sort.map((taskId, i) => {
-          const { value, isDone } = items[taskId];
-          const isSelected = taskId === selected;
+        {sort.map((itemId, i) => {
+          const { value, isDone } = items[itemId];
+          const isSelected = itemId === selected;
           return (
-            <StyledListItem onDoubleClick={() => console.log("clicked!")}>
+            <StyledListItem onDoubleClick={() => handleSelectItem(itemId)}>
               {isSelected ? (
                 <StyledTextInput type="text" value={value} />
               ) : (
@@ -45,6 +79,7 @@ const List: React.FC<ListProps> = (props) => {
             </StyledListItem>
           );
         })}
+        {/* <button onClick={handleAddNewItem}>add</button> */}
       </DragAndDrop>
     </Container>
   );
