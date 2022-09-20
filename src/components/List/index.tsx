46bd55acdcf44b9277d0abd1d3c9fe34bf9af979
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 13:53:39 -0700

    destructure props and remove editability

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,47 +1,52 @@
 import React from "react";
 import { BaseMap, ListItem } from "../../types";
 import DragAndDrop from "./DragAndDrop";
-import {
-  Checkbox,
-  StyledListItem,
-  StyledTextInput,
-} from "./DragAndDrop/styled";
+import { Checkbox, Container, StyledListItem, StyledTextInput } from "./styled";
 
 interface ListProps {
   tasks: BaseMap<ListItem>;
 }
 const List: React.FC<ListProps> = (props) => {
-  const { tasks } = props;
+  const {
+    tasks: { items, sort, selected },
+  } = props;
   return (
-    <DragAndDrop
-      dragVariants={{
-        drag: {
-          zIndex: 99,
-          position: "relative",
-          transition: {
-            duration: 0,
+    <Container>
+      <DragAndDrop
+        dragVariants={{
+          drag: {
+            zIndex: 99,
+            position: "relative",
+            transition: {
+              duration: 0,
+            },
           },
-        },
-        drop: {
-          zIndex: 0,
-        },
-      }}
-      positionTransition={{
-        type: "spring",
-        damping: 15,
-        stiffness: 100,
-      }}
-    >
-      {tasks.sort.map((taskId, i) => {
-        const { value, isDone } = tasks.items[taskId];
-        return (
-          <StyledListItem>
-            <StyledTextInput type="text" value={value} />
-            <Checkbox type="checkbox" checked={isDone} />
-          </StyledListItem>
-        );
-      })}
-    </DragAndDrop>
+          drop: {
+            zIndex: 0,
+          },
+        }}
+        positionTransition={{
+          type: "spring",
+          damping: 15,
+          stiffness: 100,
+        }}
+      >
+        {sort.map((taskId, i) => {
+          const { value, isDone } = items[taskId];
+          const isSelected = taskId === selected;
+          return (
+            <StyledListItem onDoubleClick={() => console.log("clicked!")}>
+              {isSelected ? (
+                <StyledTextInput type="text" value={value} />
+              ) : (
+                <span>{value}</span>
+              )}
+              <Checkbox type="checkbox" defaultChecked={isDone} />
+            </StyledListItem>
+          );
+        })}
+      </DragAndDrop>
+    </Container>
   );
 };
 
