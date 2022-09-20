9b04efe23e47a939db426d31e469f98787214223
Ami Cooper <ami.cooper@slalom.com>
2022-09-20 15:16:16 -0700

    add drag handle icon

---
+++ src/assets/Drag.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Drag = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 250 250"
+    fill="#525252"
+    stroke="#525252"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <title id="Drag-icon">Drag</title>
+    <circle cx="172.5" cy="47.5" r="17.5" fill="#525252"></circle>
+    <circle cx="172.5" cy="127.5" r="17.5" fill="#525252"></circle>
+    <circle cx="172.5" cy="207.5" r="17.5" fill="#525252"></circle>
+    <circle cx="82.5" cy="47.5" r="17.5" fill="#525252"></circle>
+    <circle cx="82.5" cy="127.5" r="17.5" fill="#525252"></circle>
+    <circle cx="82.5" cy="207.5" r="17.5" fill="#525252"></circle>
+  </svg>
+);
