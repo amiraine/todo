0836725190c8b48a8b26457c9a76e7568663af46
Ami Cooper <ami.cooper@slalom.com>
2022-09-21 10:37:18 -0700

    fix list item styles

--- src/List/ListItem/styled.ts
+++ src/List/ListItem/styled.ts
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
+export const IconContainer = styled.div<{ isEditable: boolean }>`
+  display: flex;
+  align-items: center;
+  opacity: ${({ isEditable }) => (isEditable ? 1 : 0)};
+  transition: 0.25 all linear;
+`;
+
 export const StyledListItem = styled.div<{ selected: boolean }>`
   display: flex;
-  justify-content: space-between;
+  align-items: center;
   padding: 5px 10px;
   background: ${({ selected }) =>
     selected ? "rgba(161, 148, 255, .5)" : "transparent"};
+
+  &:hover {
+    ${IconContainer} {
+      opacity: 1;
+      transition: 0.25 all linear;
+    }
+  }
 `;
 
 export const StyledTextInput = styled.input`
@@ -16,11 +30,47 @@ export const StyledTextInput = styled.input`
   font-size: 16px;
   padding: 0;
   border-bottom: 2px solid black;
+  height: 100%;
+
   &:focus {
     outline: 0;
   }
 `;
 
 export const Text = styled.span<{ isDone: boolean }>`
+  width: 100%;
+  height: 100%;
   text-decoration: ${({ isDone }) => (isDone ? "line-through" : "none")};
+`;
+
+export const ListItemContent = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  span {
+    margin-bottom: 2px;
+  }
+`;
+
+export const DragHandleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 12px;
+  &:hover {
+    cursor: grab;
+  }
+`;
+
+export const IconButton = styled.button`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  &:hover {
+    svg {
+      stroke: purple;
+    }
+  }
+  &:focus {
+    outline: 1px;
+  }
 `;
