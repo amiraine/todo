8879d68e3a6fb13e5ba506ee31ba77847f9f4ff9
Ami Cooper <ami.cooper@slalom.com>
2022-09-22 10:21:08 -0700

    Rename

--- src/Info/index.tsx
+++ src/Info/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container } from "./styled";
 
-const Stats: React.FC = () => {
+const Info: React.FC = () => {
   return <Container />;
 };
 
-export default Stats;
+export default Info;
