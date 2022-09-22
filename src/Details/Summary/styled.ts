79bd276dd4833393df176328498b2762b1b4995e
Ami Cooper <ami.cooper@slalom.com>
2022-09-22 13:25:48 -0700

    add padding

--- src/Details/Summary/styled.ts
+++ src/Details/Summary/styled.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 24px;
+`;
 export const InfoLine = styled.div`
   width: 100%;
   padding: 4px 8px;
