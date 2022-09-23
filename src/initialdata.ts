9e3ea21bc3ca65bae8b59af371c30b51c493eb7c
Ami Cooper <ami.cooper@slalom.com>
2022-09-22 17:55:41 -0700

    fix data

--- src/initialdata.ts
+++ src/initialdata.ts
@@ -1,4 +1,6 @@
 import { ListData } from "./types";
+import moment from "moment";
+
 export const initialCategories: string[] = [
   "Home",
   "Work",
@@ -14,20 +16,28 @@ export const initialData: ListData = {
       value: "Water plants",
       isDone: true,
       categories: [initialCategories[0]],
-      created: new Date("September 20, 2022 12:00:00").toDateString(),
+      created: moment("2022-09-20", "YYYY-MM-DD").format(),
     },
     "648ff21c-16c3-4e9d-b67a-97a78f8acaca": {
       id: "648ff21c-16c3-4e9d-b67a-97a78f8acaca",
       value: "Feed cats",
       isDone: false,
-      categories: [initialCategories[0], initialCategories[4]],
-      created: new Date("September 20, 2022 12:00:00").toDateString(),
-      due: new Date("September 20, 2022 18:00:00").toDateString(),
+      categories: [initialCategories[0], initialCategories[3]],
+      created: moment().format(),
+    },
+    "21e7fd54-1d90-4322-9d13-7b10296eea19": {
+      id: "21e7fd54-1d90-4322-9d13-7b10296eea19",
+      value: "Send cousin birthday card",
+      created: new Date().toDateString(),
+      isDone: false,
+      categories: [initialCategories[3]],
+      due: moment().add(2, "day").format(),
     },
   },
   sort: [
     "a711664b-e246-4108-a058-84fe00fb98a1",
     "648ff21c-16c3-4e9d-b67a-97a78f8acaca",
+    "21e7fd54-1d90-4322-9d13-7b10296eea19",
   ],
   selected: "a711664b-e246-4108-a058-84fe00fb98a1",
 };
