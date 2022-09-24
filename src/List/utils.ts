e2ed0c5d7ca11847c9001c2f1fc1b5445e1290ab
Ami Cooper <ami.cooper@slalom.com>
2022-09-23 23:05:50 -0700

    change util return shape

--- src/List/utils.ts
+++ src/List/utils.ts
@@ -2,9 +2,9 @@ import { ListItem } from "../types";
 
 export const categorizeListItems = (
   listItems: ListItem[]
-): { [category: string]: ListItem[] } => {
+): { [category: string]: string[] } => {
   return listItems.reduce((acc, next) => {
-    const { category } = next;
+    const { category, id } = next;
 
     if (!category) {
       // check if there is already an array for items with no categories
@@ -12,14 +12,14 @@ export const categorizeListItems = (
       // if there is, add `next`to it, otherwise initialize a new array with next as an item
       return {
         ...acc,
-        None: noCategory ? [...noCategory, next] : [next],
+        None: noCategory ? [...noCategory, id] : [id],
       };
     }
 
     const categoryArr = acc[category];
     return {
       ...acc,
-      [category]: categoryArr ? [...categoryArr, next] : [next],
+      [category]: categoryArr ? [...categoryArr, id] : [id],
     };
   }, {});
 };
