eae49121a459ae49608b35eb1ebe327ff8bebd53
Ami Cooper <ami.cooper@slalom.com>
2022-09-23 23:47:14 -0700

    revert util changs

--- src/List/utils.ts
+++ src/List/utils.ts
@@ -2,9 +2,9 @@ import { ListItem } from "../types";
 
 export const categorizeListItems = (
   listItems: ListItem[]
-): { [category: string]: string[] } => {
+): { [category: string]: ListItem[] } => {
   return listItems.reduce((acc, next) => {
-    const { category, id } = next;
+    const { category } = next;
 
     if (!category) {
       // check if there is already an array for items with no categories
@@ -12,14 +12,14 @@ export const categorizeListItems = (
       // if there is, add `next`to it, otherwise initialize a new array with next as an item
       return {
         ...acc,
-        None: noCategory ? [...noCategory, id] : [id],
+        None: noCategory ? [...noCategory, next] : [next],
       };
     }
 
     const categoryArr = acc[category];
     return {
       ...acc,
-      [category]: categoryArr ? [...categoryArr, id] : [id],
+      [category]: categoryArr ? [...categoryArr, next] : [next],
     };
   }, {});
 };
