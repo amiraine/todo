3459daa4325c61696d722f4c0df7ad0328c6a723
Ami Cooper <ami.cooper@slalom.com>
2022-09-23 23:47:02 -0700

    rm unused styles

--- src/List/ListItem/index.tsx
+++ src/List/ListItem/index.tsx
@@ -16,9 +16,6 @@ import { Trash, Copy, CornerDownRight } from "react-feather";
 import { ListItem as ListItemType, TaskState } from "../../types";
 import moment from "moment";
 import { evaluateTime } from "../../utils";
-import { useMotionValue } from "framer-motion";
-import { useBoxShadow } from "./util";
-import { ConditionalReorderItem } from "../ConditionalReorderWrappers";
 
 interface ListItemProps {
   isEditable: boolean;
@@ -45,8 +42,6 @@ const ListItem: React.FC<ListItemProps> = (props) => {
     isDraggable,
   } = props;
   const { id, value, status, due } = listItem;
-  const y = useMotionValue(0);
-  const boxShadow = useBoxShadow(y);
 
   const handleUpdateTaskState = (state: TaskState) => {
     handleUpdateItem(id, "status", TaskState);
@@ -58,50 +53,41 @@ const ListItem: React.FC<ListItemProps> = (props) => {
     distance === "FUTURE" ? moment(due).format("dddd Do MMMM YY") : fromNow;
 
   return (
-    <ConditionalReorderItem
-      useReorder={isDraggable}
-      value={value}
-      style={{ boxShadow, y }}
+    <StyledListItem
+      selected={isSelected}
+      onClick={() => handleSelectItem(id)}
+      onDoubleClick={() => setEditable(id)}
     >
-      <StyledListItem
-        selected={isSelected}
-        onClick={() => handleSelectItem(id)}
-        onDoubleClick={() => setEditable(id)}
-      >
-        <DragHandleContainer>{isDraggable && <Drag />}</DragHandleContainer>
-        <ListItemContent dueDate={due ? fromNow : ""} distance={distance}>
-          {isEditable ? (
-            <StyledTextInput
-              id={`task-item-${id}`}
-              type="text"
-              defaultValue={value}
-              onChange={(e) => handleUpdateItem(id, "value", e.target.value)}
-              autoFocus={isEditable}
-            />
-          ) : (
-            <Text isDone={status === TaskState.Complete}>{value}</Text>
-          )}
-          {!!due && (
-            <DueDateWarning fromNow={fromNow} distance={distance}>
-              <CornerDownRight size={14} />
-              {displayedDueDate}
-            </DueDateWarning>
-          )}
-          <IconContainer isEditable={isEditable}>
-            <IconButton onClick={() => handleCopyItem(id)}>
-              <Copy />
-            </IconButton>
-            <IconButton onClick={() => handleDeleteItem(id)}>
-              <Trash />
-            </IconButton>
-          </IconContainer>
-        </ListItemContent>
-        <IndeterminateCheckbox
-          value={status}
-          onChange={handleUpdateTaskState}
-        />
-      </StyledListItem>
-    </ConditionalReorderItem>
+      <DragHandleContainer>{isDraggable && <Drag />}</DragHandleContainer>
+      <ListItemContent dueDate={due ? fromNow : ""} distance={distance}>
+        {isEditable ? (
+          <StyledTextInput
+            id={`task-item-${id}`}
+            type="text"
+            defaultValue={value}
+            onChange={(e) => handleUpdateItem(id, "value", e.target.value)}
+            autoFocus={isEditable}
+          />
+        ) : (
+          <Text isDone={status === TaskState.Complete}>{value}</Text>
+        )}
+        {!!due && (
+          <DueDateWarning fromNow={fromNow} distance={distance}>
+            <CornerDownRight size={14} />
+            {displayedDueDate}
+          </DueDateWarning>
+        )}
+        <IconContainer isEditable={isEditable}>
+          <IconButton onClick={() => handleCopyItem(id)}>
+            <Copy />
+          </IconButton>
+          <IconButton onClick={() => handleDeleteItem(id)}>
+            <Trash />
+          </IconButton>
+        </IconContainer>
+      </ListItemContent>
+      <IndeterminateCheckbox value={status} onChange={handleUpdateTaskState} />
+    </StyledListItem>
   );
 };
 
